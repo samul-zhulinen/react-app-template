2dc9829a46e394a5c06e77ce6f2a500a049bf727
zhulinen <zhulinen@yuantutech.com>
2020-12-15 16:41:16 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './router';
+import 'normalize.css';
+import 'ytu-mobile/dist/index.css';
+import '@ytu-mf/ytu-mobile-biz/dist/index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
